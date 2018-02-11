05171d5d7e9ecb911ee07ba6a121b841832296a2
Bijesh Subedi <subedibijesh@gmail.com>
2018-02-11 15:25:20 -0500

    Added a ItemViewController for superhero tab

---
+++ flix_ios/View/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_ios
+//
+//  Created by Bijesh Subedi on 2/11/18.
+//  Copyright © 2018 Bijesh Subedi. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
--- flix_ios/ViewController/NowPlayingViewController.swift
+++ flix_ios/ViewController/NowPlayingViewController.swift
@@ -16,12 +16,16 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-        tableView.rowHeight = 211;
+        tableView.rowHeight = 211
         // Do any additional setup after loading the view.
+        fetchMovies()
+    }
+    
+    func fetchMovies(){
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!;
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10);
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main);
-let task = session.dataTask(with:  request) { (data, response, error) in
+        let task = session.dataTask(with:  request) { (data, response, error) in
             // When the network request returns
             if let error = error {
                 print(error.localizedDescription)
